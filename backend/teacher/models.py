1d6d6eb56afc1cf12ac0d9eb233ef9048e1b42a5
rayhan <prog.rayhan1@gmail.com>
2025-07-29 23:24:31 +0600

    feat: Teacher model created

--- backend/teacher/models.py
+++ backend/teacher/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from _applib.model_choice_fields import GenderChoice, TeacherStatus
 
-# Create your models here.
+
+class TeacherModel(models.Model):
+    phone_number = models.CharField(max_length=11, unique=True)
+    full_name = models.CharField(max_length=60)
+    profile_picture = models.CharField(max_length=300)
+    gender = models.CharField(max_length=10, choices=GenderChoice.choices, default=GenderChoice.MALE)
+    website = models.CharField(max_length=300, null=True, blank=True)
+    status = models.CharField(max_length=15, choices=TeacherStatus.choices, default=TeacherStatus.PENDING)
+    password = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"FullName: {self.full_name} || phone number: {self.phone_number} || status: {self.status}"
+    
+    class Meta:
+        verbose_name = "Teacher"
+        verbose_name_plural = "Teachers"
+        db_table = "teacher"
